8108e8c65b9788f37b960bbc05fd26c4d673d4ab
Aravindan <aravindan.dev.in@gmail.com>
2026-07-09 09:09:05 +0530

    Create 289-GameOfLife

---
+++ 289-GameOfLife/289-GameOfLife.java
@@ -0,0 +1,60 @@
+// Last updated: 7/9/2026, 9:09:03 AM
+class Solution {
+
+    public void gameOfLife(int[][] board) {
+
+        int m = board.length;
+        int n = board[0].length;
+
+        int[][] dir = {
+            {-1,-1}, {-1,0}, {-1,1},
+            {0,-1},          {0,1},
+            {1,-1},  {1,0},  {1,1}
+        };
+
+        // First pass: mark transitions
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                int live = 0;
+
+                for (int[] d : dir) {
+
+                    int r = i + d[0];
+                    int c = j + d[1];
+
+                    if (r >= 0 && r < m && c >= 0 && c < n) {
+
+                        if (board[r][c] == 1 || board[r][c] == 2)
+                            live++;
+                    }
+                }
+
+                if (board[i][j] == 1) {
+
+                    if (live < 2 || live > 3)
+                        board[i][j] = 2;   // Alive -> Dead
+
+                } else {
+
+                    if (live == 3)
+                        board[i][j] = 3;   // Dead -> Alive
+                }
+            }
+        }
+
+        // Second pass: finalize
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                if (board[i][j] == 2)
+                    board[i][j] = 0;
+
+                else if (board[i][j] == 3)
+                    board[i][j] = 1;
+            }
+        }
+    }
+}

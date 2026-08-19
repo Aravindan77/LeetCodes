3f3b093b8814898f02b5f10b7b920a7b9792078b
Aravindan <aravindan.dev.in@gmail.com>
2026-08-19 14:51:58 +0530

    Create 1386-CinemaSeatAllocation

---
+++ 1386-CinemaSeatAllocation/version/java/1386-CinemaSeatAllocation_20260819_145155.java
@@ -0,0 +1,16 @@
+// Last updated: 8/19/2026, 2:51:55 PM
+1class Solution {
+2    public int maximumProduct(int[] nums) {
+3
+4        Arrays.sort(nums);
+5
+6        int n = nums.length;
+7
+8        int p1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+9        int p2 = nums[0] * nums[1] * nums[n - 1];
+10
+11        return Math.max(p1, p2);
+12    }
+13}
+14
+15
